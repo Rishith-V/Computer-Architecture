28cbbe77efa1243bdcdc105fffbe39b93cee1291
Rishith-V <rishithvulli18@gmail.com>
2025-10-23 15:32:44 +0530

    added file for 32b reg

--- Lab5/reg32b.v
+++ Lab5/reg32b.v
@@ -1,9 +1,9 @@
 module dff (q, d, clk, res);
 
-    input d, clk;
-    output q;
+    input d, clk, res;
+    output reg q;
 
-    always @ (clk)
+    always @ (posedge clk)
         begin
             if(!res)
                 begin
@@ -19,7 +19,7 @@ endmodule
 
 module reg_32b (q, d, clk, res);
 
-    input [31:0] q;
+    input [31:0] d;
     input clk;
     input res;
 
@@ -28,10 +28,43 @@ module reg_32b (q, d, clk, res);
     genvar j;
 
     generate
-        for(j = 0; j < 32; j = j + 1);
+        for(j = 0; j < 32; j = j + 1)
             begin : reg_loop
                 dff inst (q[j], d[j], clk, res);
             end
     endgenerate
+
+endmodule
+
+module tb_reg32b;
+
+    reg [31:0] d;
+    reg clk, res;
+    wire [31:0] q;
+
+    reg_32b DUT (q, d, clk, res);
+
+    initial
+        begin
+            clk = 1'b0;
+            forever
+                begin
+                    #5 clk = 1'b1;
+                    #5 clk = 1'b0;
+                end
+        end
+
+    initial
+        begin
+            res = 1'b0;
+            #10 res = 1'b1;
+            d = 32'hafafafaf;
+            #20 $finish;
+        end
+
+    initial 
+        begin
+            $monitor($time, " : clk = %b, d = %b, res = %b, q = %b", clk, d, res, q);
+        end
 
 endmodule
